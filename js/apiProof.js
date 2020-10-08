a0a9372e093557c1ff18fb4c2b4cbb4e61c7683b
xfillmore <xuedan.fillmore@colorado.edu>
2020-10-08 00:27:15 -0600

    working with poetrydb hardcoded query

--- js/apiProof.js
+++ js/apiProof.js
@@ -1,42 +1,59 @@
 document.getElementById('querybtn').addEventListener('click', submittedQuery);
 
 function submittedQuery() {
+    console.log('received button press')
 
-    var http_request = new XMLHttpRequest();
-    http_request.open('GET', 'https://api.govinfo.gov');
-    http_request.onreadystatechange = function() {
-        if (this.status == 200 && this.readyState == 4) {
-            console.log ('response: ' + this.responseText);
+    // var http_request = new XMLHttpRequest();
+    // http_request.open('GET', 'https://poetrydb.org/');
+    // http_request.onreadystatechange = function() {
+    //     if (this.status == 200 && this.readyState == 4) {
+    //         console.log ('response: ' + this.responseText);
+    //     }
+    // };
+    // // http_request.setRequestHeader('Content-Type', 'application/json');
+    // http_request.send()
+
+    // try XMLHttpRequest instead...
+    var req;
+    if (window.XMLHttpRequest) {
+        req = new XMLHttpRequest();
+        console.log('created new xmlhttprequest object');
+    }
+    else {
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    req.open('GET', 'https://poetrydb.org/author');
+    req.onreadystatechange = function () {
+        console.log('readystatechange');
+        if ((this.readyState == 4) && (this.status == 200)) {
+            var resp = JSON.parse(req.responseText);
+            // console.log(req.responseText);
+            displayReturn(resp.authors);
         }
     };
-    http_request.withCredentials = true;
-    // http_request.setRequestHeader('Content-Type', 'application/json');
-    http_request.send()
+    req.send();
 
-    // var specs = 'packages/CREC-2018-01-04/summary/';
-    var specs = 'collections/CREC/2018-10-10/summary/';
-    // var url = new URL('https://api.govinfo.gov/' + specs + '?api_key=' + apiKey);
-    var url = new URL('https://api.govinfo.gov/' + specs);
 
-    console.log(url);
+    // var specs = 'author';
+    // var url = new URL('https://poetrydb.org/' + specs);
 
-    var parameters = {
-        // pageSize: 10,
-        // offset: 0,
-        api_key: apiKey
-    }
+    // console.log(url);
 
-    // url.searchParams.append(parameters); // = new URLSearchParams(parameters);
-    url.search = new URLSearchParams(parameters);
-
-    fetch(url, {mode: 'no-cors'})
-        .then(response => response.json())
-        .then(data => displayReturn(data))
-        .catch(console.log('Failed to fetch data.'))
+    // fetch(url, {mode: 'no-cors'})
+    //     .then(response => response.json())
+    //     .then(data => displayReturn(data))
+    //     .catch(console.log('Failed to fetch data.'))
 
 }
 
 function displayReturn(data) {
-    document.getElementById('data').innerHTML = data; // raw json
-    cosole.log('displayed data');
+    document.getElementById('data').innerHTML = '<ul>';
+    for (var i = 0; i < data.length; i++) {
+        document.getElementById('data').innerHTML += '<li>'+data[i]+'</li>'; // raw json
+    }
+    document.getElementById('data').innerHTML += '</ul>';
+
+    document.getElementById('data').innerHTML += "<p>Can you believe they don't have Robert Frost, Sylvia Plath, and Maya Angelou?";
+
+    console.log('displayed data');
 }
